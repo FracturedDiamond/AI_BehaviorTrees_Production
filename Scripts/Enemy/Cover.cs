dc30d5ba01c6093342c969e1aab4a0a3678e338a
FracturedDiamond <49692399+FracturedDiamond@users.noreply.github.com>
2021-03-29 09:56:44 -0700

    Add files via upload

---
+++ Scripts/Enemy/Cover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cover : MonoBehaviour
+{
+    [SerializeField] private Transform[] coverSpots;
+
+    public Transform[] GetCoverSpots()
+    {
+        Debug.Log("Cover.cs");
+        return coverSpots;
+    }
+
+}
